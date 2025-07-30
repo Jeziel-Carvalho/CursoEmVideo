b01dcb9dbaf72b873014aca7e5e7728157aea1e7
Jeziel Carvalho Vilas Bôas <thesummerjez@gmail.com>
2025-07-30 15:18:14 -0300

    Modificação do 5 pelo 10
    Call com a Juh

--- EX028.py
+++ EX028.py
@@ -2,8 +2,8 @@
 # qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
 from random import randrange
-numero = randrange(0,5) #Computador escolhe um número
-print ('Estou pensando em um número entre 0 e 5.\n Consegue adivinhar?')
+numero = randrange(0,10) #Computador escolhe um número
+print ('Estou pensando em um número entre 0 e 10.\n Consegue adivinhar?')
 resposta = int(input('Eu acho que o número é... ')) #Jogador adivinha o número
 if numero == resposta:
     print(f'Você acertou, o número era {numero} PARABÉNS!!')
