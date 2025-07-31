4e9d3f0b27daa6f902c3b222ba22191a91a6504a
Jeziel Carvalho Vilas Bôas <thesummerjez@gmail.com>
2025-07-31 16:24:32 -0300

    Condição 1
    Convertendo em binário

--- EX037.py
+++ EX037.py
@@ -2,3 +2,23 @@
 #1 para binário
 #2 para octal
 #3 para hexadecimal
+
+#Seletor de conversor
+num = int(input('Escreva um número inteiro: '))
+print ('''Escolha uma das opções abaixo
+       [1]Binário
+       [2]Octal
+       [3]Hexadecimal''')
+opção = int(input('Sua opção: '))
+
+#Condição aninhada (binário)
+if opção == 1:
+    binario = ''
+    while num > 0:
+        resto = num % 2
+        binario = str(resto) + binario
+        num = num // 2
+print(f'O número {num} em binário é: {binario}')
+
+elif opção == 2:
+    octal = ''
