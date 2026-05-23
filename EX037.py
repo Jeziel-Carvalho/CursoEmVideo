36f40434a529fc459d347a839ba33fac90cc9381
Jeziel Carvalho Vilas Bôas <thesummerjez@gmail.com>
2026-05-23 20:48:03 -0300

    Update EX037.py

--- EX037.py
+++ EX037.py
@@ -9,16 +9,15 @@ print ('''Escolha uma das opções abaixo
        [1]Binário
        [2]Octal
        [3]Hexadecimal''')
-opção = int(input('Sua opção: '))
+opcao = int(input('Sua opção: '))
 
 #Condição aninhada (binário)
-if opção == 1:
+if opcao == 1:
     binario = ''
     while num > 0:
         resto = num % 2
         binario = str(resto) + binario
         num = num // 2
-print(f'O número {num} em binário é: {binario}')
-
-elif opção == 2:
-    octal = ''
+    print(f'O número {num} em binário é: {binario}')
+elif opcao == 2:
+    
